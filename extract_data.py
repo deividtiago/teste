466dce7fd1939181db2769ca7a2ab6f414b33fb4
deividtiago <94766526+deividtiago@users.noreply.github.com>
2025-08-09 17:05:09 -0300

    Update extract_data.py

--- extract_data.py
+++ extract_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 from io import StringIO
 
-url = "https://huggingface.co/datasets/kestra/datasets/raw/main/csv/orders.csv
+url = "https://huggingface.co/datasets/kestra/datasets/raw/main/csv/orders.csv"
 response = requests.get(url)
 df = pd.read_csv(StringIO(response.text))
 
