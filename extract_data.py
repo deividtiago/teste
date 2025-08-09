fa52fbb1a16a6a0e47eb7bdd5181f827a692194c
deividtiago <94766526+deividtiago@users.noreply.github.com>
2025-08-09 16:55:59 -0300

    Create extract_data.py

---
+++ extract_data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import requests
+from io import StringIO
+
+url = "https://huggingface.co/datasets/kestra/datasets/raw/main/csv/orders.csv
+response = requests.get(url)
+df = pd.read_csv(StringIO(response.text))
+
+df.to_csv("raw_data.csv", index=False)
